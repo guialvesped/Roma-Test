a8501ef6983b05be2753da82d73e24f58440e3fc
Guilherme Alves Pedroso <alvespedrosog@gmail.com>
2024-09-17 01:37:22 -0300

    Fixes
    I fixed the side image position

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 //import imgSide from "../src/assets/img/imgSide.png"
 import './App.css'
 import Form from './components/Form'
-import imgLat from './assets/img/imgSide.png'
 
 function App() {
 
   return (
     <>
     <div className='imgLat'>
-      <img src={imgLat} alt="" />
+      
     </div>
-    <Form/>
+    <div>
+      <Form/>
+    </div>
     </>
   )
 }
